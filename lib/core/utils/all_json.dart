b216c6abea84d8fab900b0a4721dcdea0ceb7535
Alona Laskar <alona159951@gmail.com>
2024-12-19 16:40:00 +0200

    Add "detail" elemant to hotelList

--- lib/core/utils/all_json.dart
+++ lib/core/utils/all_json.dart
@@ -89,42 +89,91 @@ List<Map<String, dynamic>> hotelList = [
     'image': 'hotel2.jpg',
     'place': 'One Space',
     'deriction': 'London',
-    'price': '25'
+    'price': '25',
+    'detail':
+        'Nestled in the heart of the city, Grand Horizon Hotel offers a perfect blend of luxury, comfort, and convenience for travelers from around the world. This five-star retreat boasts elegantly designed rooms and suites, each equipped with modern amenities like high-speed Wi-Fi, plush bedding, and panoramic windows offering breathtaking views of the city skyline. Guests can indulge in a variety of dining experiences, from gourmet international cuisine at the rooftop restaurant to relaxing cocktails at the stylish lobby lounge.',
+    'images': [
+      'pool_view2.jpg',
+      'pool_view4.jpg',
+      'pool_view3.jpg',
+    ]
   },
   {
     'image': 'hotel3.jpg',
     'place': 'Golden Will',
     'deriction': 'London',
-    'price': '40'
+    'price': '40',
+    'detail':
+        'Skyline Inn offers an incredible urban escape in the heart of Manhattan, featuring rooms with panoramic views of the city skyline. Guests can enjoy high-speed internet, luxurious bedding, and a rooftop bar serving signature cocktails. Located near Times Square, Broadway, and Central Park, the hotel provides a central hub for exploring the vibrant energy of New York City. Its modern decor and thoughtful amenities cater to both business and leisure travelers, ensuring a seamless and unforgettable stay.',
+    'images': [
+      'pool_view.jpg',
+      'pool_view3.jpg',
+      'desert_view.jpg',
+    ]
   },
   {
     'image': 'hotel4.jpg',
     'place': 'Skyline Inn',
     'deriction': 'New York',
-    'price': '50'
+    'price': '50',
+    'detail':
+        'Sea Breeze is a tropical haven located right on the shores of Miami Beach, offering guests a relaxing retreat. With ocean-facing rooms, a vibrant pool area surrounded by palm trees, and direct access to the beach, it’s a dream destination for sun-seekers. Guests can explore the nightlife of South Beach, enjoy local seafood at the hotel’s restaurant, or indulge in a range of water activities. The friendly and welcoming atmosphere makes Sea Breeze perfect for both families and solo travelers.',
+          'images': [
+      'pool_view2.jpg',
+      'pool_view3.jpg',
+      'pool_view.jpg',
+    ]
   },
   {
     'image': 'hotel5.jpg',
     'place': 'Sea Breeze',
     'deriction': 'Miami',
-    'price': '35'
+    'price': '35',
+    'detail':
+        'Nestled in the Rockies, Mountain Retreat provides a serene escape with breathtaking views of the surrounding mountains. This rustic yet luxurious lodge features cozy cabins equipped with fireplaces and large windows that let in the beauty of nature. Guests can enjoy guided hikes, skiing in the winter, or relaxing evenings by the outdoor fire pits. The on-site restaurant offers hearty meals made with fresh, local ingredients, making it the perfect getaway for nature lovers and adventurers alike.',
+        'images': [
+      'pool_view4.jpg',
+      'desert_view4.jpg',
+      'desert_view3.jpg',
+    ]
   },
   {
     'image': 'hotel6.jpg',
     'place': 'Mountain Retreat',
     'deriction': 'Denver',
-    'price': '60'
+    'price': '60',
+    'detail':
+        'Urban Haven is a contemporary hotel in Tokyo, blending sleek design with traditional Japanese hospitality. The rooms feature tatami-style options and modern amenities like high-speed Wi-Fi and smart TVs. Guests can experience the relaxing onsen baths or enjoy authentic Japanese cuisine at the in-house sushi bar. Its central location offers easy access to Shibuya, Ginza, and Akihabara, making it a top choice for travelers eager to immerse themselves in Tokyo’s vibrant culture and lifestyle.',
+    'images': [
+      'pool_view2.jpg',
+      'desert_view2.jpg',
+      'desert_view3.jpg',
+    ]
   },
   {
     'image': 'hotel7.jpg',
     'place': 'Urban Haven',
     'deriction': 'Tokyo',
-    'price': '70'
+    'price': '70',
+    'detail':
+        'Royal Palm is a luxurious five-star hotel in Dubai, offering unparalleled elegance and world-class service. The hotel features opulent suites with panoramic views of the Dubai skyline and the Arabian Gulf. Guests can relax at the rooftop infinity pool, dine at gourmet international restaurants, and indulge in spa treatments tailored to rejuvenate the mind and body. Located within walking distance of the Burj Khalifa and Dubai Mall, Royal Palm combines convenience with lavish comfort for an unforgettable experience.',
+    'images': [
+      'pool_view.jpg',
+      'desert_view3.jpg',
+      'desert_view.jpg',
+    ]
   },
   {
     'image': 'hotel8.jpg',
     'place': 'Royal Palm',
     'deriction': 'Dubai',
-    'price': '120'
+    'price': '120',
+    'detail':
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat Royal Palm is a luxurious five-star hotel in Dubai, offering unparalleled elegance and world-class service. The hotel features opulent suites with panoramic views of the Dubai skyline and the Arabian Gulf. Guests can relax at the rooftop infinity pool, dine at gourmet international restaurants, and indulge in spa treatments tailored to rejuvenate the mind and body. Located within walking distance of the Burj Khalifa and Dubai Mall, Royal Palm combines convenience with lavish comfort for an unforgettable experience.',
+    'images': [
+      'pool_view.jpg',
+      'pool_view3.jpg',
+      'desert_view.jpg',
+    ]
   }
 ];
